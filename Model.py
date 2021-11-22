d50a9b2b9acee18bfe366481160a1142d9bdeefd
Mohamed EL BARCANI <melbarcani@github.com>
2021-11-22 16:12:18 +0100

    take just regular cells

--- Model.py
+++ Model.py
@@ -33,7 +33,7 @@ REWARD_FILLED_CELL = -10
 
 def create_board(rows, cols):
     matrix = np.arange(14 * 8).reshape(rows, cols)
-    # matrix = np.zeros((rows, cols))
+    #matrix = np.zeros((rows, cols))
     matrix[ROWS_NB - 1] = GROUND
     matrix[:, 0] = BORDER
     matrix[:, cols - 1] = BORDER
@@ -84,9 +84,11 @@ def get_nearest_cells(board, row, col):
 
 def get_nearest_cells_recursive_helper(board, row, col, row_count, array):
     if row_count < 2 and row == ROWS_NB - 2 and len(array) < 3:
-        get_cells_when_too_close_to_ground(array, board, col, row)
+        print()
+        # get_cells_when_too_close_to_ground(array, board, col, row)
     elif row_count < 2 and row == ROWS_NB - 2 and len(array) == 3:
-        array.append(package_right_and_left_four_cells(board, row, col))
+        print()
+        # array.append(package_right_and_left_four_cells(board, row, col))
     elif row_count < 2 and row < ROWS_NB - 2:
         get_nearest_cells_recursive_helper(board, row + 1, col, row_count + 1, array)
         get_regular_cells(array, board, col, row, row_count)
@@ -140,36 +142,39 @@ class Environment:
         print(self.__states)
         print(board)
 
+    @property
+    def states(self):
+        return self.__states.keys()
 
-def apply(self, agent, action):
-    state = agent.state
-    if action == DOWN:
-        new_state = (state[0] + 1, state[1])
-    elif action == LEFT:
-        new_state = (state[0], state[1] - 1)
-    elif action == RIGHT:
-        new_state = (state[0], state[1] + 1)
-    elif action == CHANGE:
-        agent.column[0], agent.column[1], agent.column[2] = agent.column[2], agent.column[0], agent.column[1]
-        new_state = ((state[0], state[1]), agent.column)
-    else:
-        raise 'Unknown action'
-    if new_state in self.__states:
-        state = new_state
-        if self.__states[new_state] == BORDER:
-            reward = REWARD_BORDER
-        elif self.__states[new_state] == GROUND:
-            reward = check_clear_color()
+    def apply(self, agent, action):
+        state = agent.state
+        if action == DOWN:
+            new_state = (state[0] + 1, state[1])
+        elif action == LEFT:
+            new_state = (state[0], state[1] - 1)
+        elif action == RIGHT:
+            new_state = (state[0], state[1] + 1)
+        elif action == CHANGE:
+            agent.column[0], agent.column[1], agent.column[2] = agent.column[2], agent.column[0], agent.column[1]
+            new_state = ((state[0], state[1]), agent.column)
+        else:
+            raise 'Unknown action'
+        if new_state in self.__states:
+            state = new_state
+            if self.__states[new_state] == BORDER:
+                reward = REWARD_BORDER
+            elif self.__states[new_state] == GROUND:
+                reward = check_clear_color()
 
 
-@property
-def states(self):
-    return self.__states.keys()
+    @property
+    def states(self):
+        return self.__states.keys()
 
 
 class Agent:
     def __init__(self, environment, learning_rate=0.4, discount_factor=0.5):
-        self.__states = None
+        self.__state = None
         self.__column = None
         self.__environment = environment
         self.__learning_rate = learning_rate
@@ -180,13 +185,24 @@ class Agent:
             for a in ACTIONS:
                 self.__qtable[s][a] = 0.0
 
-    def update(self, column):
-        self.__states[(3, ROWS_NB - 3)] = column[2]
-        self.__states[(2, ROWS_NB - 2)] = column[1]
-        self.__states[(1, ROWS_NB - 1)] = column[0]
+    def update(self, state, action, reward):
+        # Q(s, a) <- Q(s, a) + learning_rate *
+        #                     [reward + discount_factor * max(Q(state)) - Q(s, a)]
+
+        maxQ = max(self.__qtable[state].values())
+        self.__qtable[self.__state][action] += self.__learning_rate * \
+                                               (reward + self.__discount_factor * \
+                                                maxQ - self.__qtable[self.__state][action])
+        print("state", state)
+        self.__state = state
+        self.__score += reward
 
     def reset(self):
         self.__score = 0
+
+    @property
+    def state(self):
+        return self.__state
 
     @property
     def column(self):
@@ -202,6 +218,6 @@ if __name__ == '__main__':
     agent = Agent(env)
 
     for i in range(2):
-        env.update([RED, RED, GREEN])
+        print("ok")
         # while True:
         #   agent.column = [GREEN, GREEN, RED]
